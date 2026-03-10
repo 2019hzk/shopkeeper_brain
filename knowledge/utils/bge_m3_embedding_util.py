295353cb456f9cf89c6afb47335eab5fed8c13a5
huzhongkui <2595753724@qq.com>
2026-03-10 17:36:26 +0800

    day11-查询流程混合检索的使用

--- knowledge/utils/bge_m3_embedding_util.py
+++ knowledge/utils/bge_m3_embedding_util.py
@@ -1,5 +1,5 @@
 import os, logging
-from typing import Optional
+from typing import Optional,List
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -37,22 +37,47 @@ def get_beg_m3_embedding_model():
     return bge_m3_ef
 
 
-if __name__ == '__main__':
-    embedding_model = get_beg_m3_embedding_model()
 
-    query = "我喜欢Python语言"
+def generate_hybrid_embeddings(embedding_model: BGEM3EmbeddingFunction, embedding_documents: List[str]):
+    """
+    为文本生成向量嵌入
+    :param embedding_model: 嵌入模型(这里使用BGEM3)
+    :param embedding_documents: 要生成嵌入的文本列表
+    :return: 包含dense和sparse向量的字典
+    """
+    try:
+        # 1. 生成嵌入
+        embedding_result = embedding_model.encode_documents(embedding_documents)
 
-    # print(embedding_model.encode_queries([query]))
-    result = embedding_model.encode_documents([query])
-    print(result)
+        processed_sparse_result = []
+        # 2. 遍历每一个文档
+        for index in range(len(embedding_documents)):
+            # 2.1 解构csr矩阵&获取稀疏向量
+            csr_array = embedding_result['sparse']
+            # a) 行索引
+            ind_ptr = csr_array.indptr
 
-    # 稠密向量：
-    dense = result['dense'][0].tolist()
+            # b) 获取行索引的起始值
+            start_ind_ptr = ind_ptr[index]
+            end_ind_ptr = ind_ptr[index + 1]
 
-    # 稀疏向量（CSR:核心目标：将整个空间那些非0的元素存储起来:行指针[indptr](0 6) 权重列表[data](0.01,0.21,0.13,0.04,0.5,0.6) tokenId 列表[indices](1000,900,10,1,2,9999)）
-    # Mivlus:sparse:{"tokenId":'weight',....}
-    print(result['sparse'].indptr[0])
-    print(result['sparse'].indptr[1])
-    weights = result['sparse'].data[0:6].tolist()
-    tokenIds = result['sparse'].indices[0:6].tolist()
-    print(dict(zip(tokenIds, weights)))
+            # c) 获取token_id
+            token_id = csr_array.indices[start_ind_ptr:end_ind_ptr].tolist()
+
+            # d) 获取权重
+            weight = csr_array.data[start_ind_ptr:end_ind_ptr].tolist()
+
+            # 2.2 获取稀疏向量
+            sparse_vector = dict(zip(token_id, weight))
+
+            processed_sparse_result.append(sparse_vector)
+
+        # 3. 返回
+        return {
+            "dense": [den.tolist() for den in embedding_result["dense"]],
+            "sparse": processed_sparse_result
+        }
+    except Exception as e:
+        return None
+
+
